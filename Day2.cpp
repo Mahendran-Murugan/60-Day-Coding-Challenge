91fe3f12d0655bc0909a827edc9eead4aeb24575
mahi000007 <mahendran1482004@gmail.com>
2024-01-31 21:06:27 +0530

    chore: comments fixed

--- Day2.cpp
+++ Day2.cpp
@@ -2,26 +2,32 @@
 Day 2
 Valid Parentheses
 Type - EASY
-Given a strings containing just the characters '(' , ')' , '{' , '}' , '['&']' ,determine if the input string is valid. 
-An input string is valid if: 
-Open brackets must be closed by the same type of brackets. 
-Open brackets must be closed in the correct order. 
+Given a strings containing just the characters '(' , ')' , '{' , '}' , '['&']' ,determine if the input string is valid.
+An input string is valid if:
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
 Every close bracket has a corresponding open bracket of the same type.
 
 
-Asked in Google, Amazone, Microsoft, Paypal
+Asked in Google, Amazon, Microsoft, Paypal
 */
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-string validBrackets(string str){
-    stack <char> st;
-    for(int i = 0; i < str.length(); i++){
-        if(str[i] == '(' || str[i] == '{' || str[i] == '['){
+string validBrackets(string str)
+{
+    stack<char> st;
+    for (int i = 0; i < str.length(); i++)
+    {
+        if (str[i] == '(' || str[i] == '{' || str[i] == '[')
+        {
             st.push(str[i]);
-        }else{
-            if(st.empty() || str[i] == ')' && st.top() != '(' || str[i] == '}' && st.top() != '{' || str[i] == ']' && st.top() != '['){
+        }
+        else
+        {
+            if (st.empty() || str[i] == ')' && st.top() != '(' || str[i] == '}' && st.top() != '{' || str[i] == ']' && st.top() != '[')
+            {
                 return "Not Valid";
             }
             st.pop();
@@ -30,7 +36,8 @@ string validBrackets(string str){
     return "Valid";
 }
 
-int main(){
+int main()
+{
     string str = "{}([])";
     cout << validBrackets(str) << endl;
     str = "{{{{]]]}})";
